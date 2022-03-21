35f35ed7d3cece18e19424b36a90617bfcb3b08f
mpt <f.n.heravi@gmail.com>
2022-03-21 17:45:15 +0100

    Added cartesian_ik_node
    - it gets a Target Point from a specific topic
    - performs the inverse kinematics
    - publishes the corresponding position command on each cartesian joint controller
    
    TODO:
    - implement the IK formulas in the function
    - specify the exact subscription topic name
    - test it in gazebo

--- src/cartesian_robot/src/cartesian_ik.cpp
+++ src/cartesian_robot/src/cartesian_ik.cpp
@@ -3,16 +3,48 @@
 // published the required joint commands.
 
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/Point.h>
 #include <boost/foreach.hpp>
 
-void subs_callback(const std_msgs::StringPtr& msgs ){
-    ROS_INFO_STREAM("MSG: " << msg);
+
+std::vector<ros::Publisher> joint_pubs;
+
+void publish_command(std::vector<double> &pos, std::vector<double> &vel, std::vector<double> &cur){
+    // sensor_msgs::JointState cmd;
+    // std_msgs::Header header = std_msgs::Header();
+    // header.stamp = ros::Time::now();
+    // cmd.header = header;
+    // cmd.position = pos;
+    // cmd.velocity = vel;
+    // cmd.effort = cur;
+    // joint_pubs[0].publish(cmd);
+
+    // TODO temporary for only position controllers
+    int i = 0;
+    BOOST_FOREACH(double position, pos)
+    {
+        std_msgs::Float64 cmd;
+        cmd.data = position;
+        joint_pubs[i].publish(cmd);
+        i++;
+    }
 }
 
-void cartesian_ik(){
-    return;
+void cartesian_ik(const double &x, const double &y, const double &theta){
+    // calculate joint1, joint2, and joint3 positions
+    std::vector<double> temp = {x,y,theta};
+    std::vector<double> zero = {0,0,0};
+    publish_command(temp,zero,zero);
+}
+
+void subs_callback(const geometry_msgs::PointConstPtr &msg ){ // TODO message type
+    ROS_INFO_STREAM("Target Point: [" << msg->x << ", " << msg->y << ", " << msg->z << "]");
+    // call inverse kinematics functions
+    cartesian_ik(msg->x, msg->y, msg->z);
 }
 
 int main(int argc, char *argv[]){
@@ -26,7 +58,6 @@ int main(int argc, char *argv[]){
     
     // define node handlse and publishers for each joint
     std::vector<ros::NodeHandle> joint_nhs;
-    std::vector<ros::Publisher> joint_pubs;
     BOOST_FOREACH(const std::string &motor_name, motor_names)
     {
         ros::NodeHandle joint_nh(robot_nh, motor_name);
@@ -38,8 +69,12 @@ int main(int argc, char *argv[]){
 
     // set subscriber to get target points
     // CONFIG
-    ros::Subscriber sub = nh.subscribe("topicname", 10, subs_callback);
-
-
+    ros::Subscriber sub = nh.subscribe<geometry_msgs::Point>("topicname", 10, subs_callback);
+    ros::Rate rate(50);
+    while(ros::ok())
+    {
+        ros::spinOnce();
+        rate.sleep();
+    }
     return 1;
 }
