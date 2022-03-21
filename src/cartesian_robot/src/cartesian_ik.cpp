850e72faf9f5198e7b1740096ebaa879d74aea42
mpt <f.n.heravi@gmail.com>
2022-03-21 21:46:46 +0100

    Added motors parameters to rosparam. Modified IK node. Added motor.yaml file.

--- src/cartesian_robot/src/cartesian_ik.cpp
+++ src/cartesian_robot/src/cartesian_ik.cpp
@@ -12,6 +12,7 @@
 
 
 std::vector<ros::Publisher> joint_pubs;
+std::vector<double> joints_ratio;       // ratio: linear motion / rotation motion
 
 void publish_command(std::vector<double> &pos, std::vector<double> &vel, std::vector<double> &cur){
     // sensor_msgs::JointState cmd;
@@ -36,31 +37,53 @@ void publish_command(std::vector<double> &pos, std::vector<double> &vel, std::ve
 
 void cartesian_ik(const double &x, const double &y, const double &theta){
     // calculate joint1, joint2, and joint3 positions
-    std::vector<double> temp = {x,y,theta};
+    // convert x, y, z to motor rotation angles in rad.
+    std::vector<double> temp;
+
+
+    // TODO correct these
+    temp.push_back( x / joints_ratio[0] );
+    temp.push_back( y / joints_ratio[1] );
+    temp.push_back( theta );
+    // TODO apply offsetss
+    
     std::vector<double> zero = {0,0,0};
+    // publish commands
     publish_command(temp,zero,zero);
 }
 
 void subs_callback(const geometry_msgs::PointConstPtr &msg ){ // TODO message type
-    ROS_INFO_STREAM("Target Point: [" << msg->x << ", " << msg->y << ", " << msg->z << "]");
+    ROS_DEBUG_STREAM("Target Point: [" << msg->x << ", " << msg->y << ", " << msg->z << "]");
     // call inverse kinematics functions
     cartesian_ik(msg->x, msg->y, msg->z);
 }
 
 int main(int argc, char *argv[]){
     ros::init(argc, argv, "cartesian_robot_ik");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh;     // since it is running in a group. nh is not the root node handle. it is /name_space
 
-
-    // TODO read robot and joint names from rosparam to create corresponding node handles.
-    ros::NodeHandle robot_nh(nh, "cartesian_");
-    std::vector<std::string> motor_names = {"joint1", "joint2", "joint3"};
+    // get joint names from rosparam
+    std::vector<std::string> motor_names;
+    std::string joint_name;
+    double joint_ratio;
+    nh.param<std::string>("joint1", joint_name, "joint1");
+    nh.param<double>("joint1/ratio", joint_ratio, 1);
+    motor_names.push_back(joint_name);
+    joints_ratio.push_back(joint_ratio);
+    nh.param<std::string>("joint2", joint_name, "joint2");
+    nh.param<double>("joint2/ratio", joint_ratio, 1);
+    joints_ratio.push_back(joint_ratio);
+    motor_names.push_back(joint_name);
+    nh.param<std::string>("joint3", joint_name, "joint3");
+    nh.param<double>("joint3/ratio", joint_ratio, 1);
+    motor_names.push_back(joint_name);
+    joints_ratio.push_back(joint_ratio);
     
     // define node handlse and publishers for each joint
     std::vector<ros::NodeHandle> joint_nhs;
     BOOST_FOREACH(const std::string &motor_name, motor_names)
     {
-        ros::NodeHandle joint_nh(robot_nh, motor_name);
+        ros::NodeHandle joint_nh(nh, motor_name);
         joint_nhs.push_back( joint_nh );
 
         ros::Publisher pub = joint_nh.advertise<std_msgs::Float64>("position_controller/command",10);
@@ -68,9 +91,10 @@ int main(int argc, char *argv[]){
     }
 
     // set subscriber to get target points
-    // CONFIG
-    ros::Subscriber sub = nh.subscribe<geometry_msgs::Point>("topicname", 10, subs_callback);
-    ros::Rate rate(50);
+    // CONFIG topicname
+    std::string target_point_topic = "/cartesian_/TargetPoint";
+    ros::Subscriber sub = nh.subscribe<geometry_msgs::Point>(target_point_topic, 10, subs_callback);
+    ros::Rate rate(200);
     while(ros::ok())
     {
         ros::spinOnce();
